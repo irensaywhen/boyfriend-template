7e21d35aee6dba70a63c63b91cc454d56d539875
unknown <vooriden@gmail.com>
2020-03-07 03:24:18 +0100

    fixes

--- js/validation.js
+++ js/validation.js
@@ -4,8 +4,7 @@ reg = {
         return new RegExp('\\S+@\\S+\\.\\S+')
     },
     name : function(min,max){
-        return new RegExp(`^[a-zA-Z0-9]{${min},${max}}$`);
-       
+        return new RegExp(`^[a-zA-Z0-9]{${min},${max}}$`);  
     },
     password: function(min,max){
         return new RegExp(`^.{${min},${max}}$`)
@@ -28,4 +27,21 @@ function activateLabels(){
 }
 function deactivateLabels(){
     $('.form-control-label').removeClass('activeLabel')
+}
+function validateDate(day,month,year){
+
+    let age = 18;
+    let setDate = new Date(year + age, month , day);
+    let currdate = new Date();
+    
+    if (currdate >= setDate) {
+      // you are above 18
+      return true
+    } else {
+      return false
+    }
+}
+function validateLocation(location, array){
+   
+    return array.findIndex(item => location.toLowerCase() === item.toLowerCase()) != -1;
 }
