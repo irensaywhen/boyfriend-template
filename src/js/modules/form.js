8683e3fb80e57e37e19cec2e6bd93a05bf161e73
Irina <irensaywhen@gmail.com>
2020-07-10 18:08:53 +0300

    Started form and location implementation, collecting form data setup

--- src/js/modules/form.js
+++ src/js/modules/form.js
@@ -4,9 +4,49 @@ import location from "./locationMixin.js";
 export default class Form extends ServerRequest {
   constructor(options) {
     super(options);
+
+    // Data that will be sent to the server
+    this.formData = {};
+
+    // Bind context
+    this.cacheElements = this.cacheElements.bind(this);
+    this.setUpEventListeners = this.setUpEventListeners.bind(this);
+
+    if (options.location) {
+      // Add methods to the form object
+      Object.assign(Form.prototype, location);
+      this.location = true;
+    }
+
+    this.cacheElements();
+    this.setUpEventListeners();
   }
 
-  cacheElements() {}
+  cacheElements() {
+    // Form
+    this.$form = $(this.selectors.form);
 
-  setUpEventListeners() {}
+    // Input fields
+    this.$inputs = this.$form.find(this.selectors.inputs);
+
+    if (this.location) {
+      this.initializeLocationInput();
+    }
+  }
+
+  setUpEventListeners() {
+    this.$form.submit((event) => {
+      event.preventDefault();
+      event.stopPropagation();
+
+      // Collect form inputs
+      this.$inputs.each((index, element) => {
+        let name = element.name;
+
+        this.formData[name] = $(element).val();
+      });
+    });
+  }
+
+  collectFormData() {}
 }
--- src/js/modules/locationMixin.js
+++ src/js/modules/locationMixin.js
@@ -1,1 +1,34 @@
-export default {};
+export default {
+  initializeLocationInput() {
+    let requestInfo = this.requests.location;
+    // Cache input element
+    this.$locationInput = this.$form.find(this.selectors.locationInput);
+
+    // Add default query params
+    for (let key in requestInfo.searchParams) {
+      requestInfo.endpoint.searchParams.set(key, requestInfo.searchParams[key]);
+    }
+
+    // Add event listener
+    this.$locationInput.on("input", (event) => {
+      // Specify the inputed city
+      requestInfo.endpoint.searchParams.set("city", this.$locationInput.val());
+
+      // Make request to the server
+      this.getCities({
+        headers: requestInfo.headers,
+        endpoint: requestInfo.endpoint,
+        method: requestInfo.method,
+      });
+    });
+  },
+
+  async getCities({ headers, endpoint, method }) {
+    let result = await this.makeRequest({
+      headers,
+      endpoint,
+      method,
+    });
+    console.log(result);
+  },
+};
