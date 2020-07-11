d3d4c53821cdc210adf1f49915b50cb1c1bff6a7
Irina <irensaywhen@gmail.com>
2020-07-11 12:19:03 +0300

    location preview added

--- src/js/modules/locationMixin.js
+++ src/js/modules/locationMixin.js
@@ -1,34 +1,140 @@
 export default {
+  locationInputStarted: false,
+  locationInputValue: null,
+
   initializeLocationInput() {
     let requestInfo = this.requests.location;
-    // Cache input element
-    this.$locationInput = this.$form.find(this.selectors.locationInput);
+
+    // Bind context
+    this.throttleInput = this.throttleInput.bind(this);
+    this.displayCities = this.displayCities.bind(this);
 
     // Add default query params
     for (let key in requestInfo.searchParams) {
       requestInfo.endpoint.searchParams.set(key, requestInfo.searchParams[key]);
     }
 
-    // Add event listener
-    this.$locationInput.on("input", (event) => {
-      // Specify the inputed city
-      requestInfo.endpoint.searchParams.set("city", this.$locationInput.val());
+    /* Elements caching and event listeners initialization */
+    this.cacheLocationElements();
+    this.setUpLocationEventListeners();
+  },
 
-      // Make request to the server
-      this.getCities({
-        headers: requestInfo.headers,
-        endpoint: requestInfo.endpoint,
-        method: requestInfo.method,
-      });
+  cacheLocationElements() {
+    // Cache input element
+    this.$locationInput = this.$form.find(this.selectors.locationInput);
+
+    // location dropdown wrapper
+    this.$locationDropdownWrapper = this.$form.find(
+      this.selectors["location-dropdown"]
+    );
+
+    // Dropdown toggle
+    this.$locationDropdownToggle = this.$locationDropdownWrapper.find(
+      this.selectors["dropdown-toggle"]
+    );
+
+    // Dropdown menu
+    this.$locationDropdownMenu = this.$locationDropdownWrapper.find(
+      this.selectors["dropdown-menu"]
+    );
+  },
+
+  setUpLocationEventListeners() {
+    // Listen to typing event
+    this.$locationInput.on("input", (event) => {
+      // If the user selects the city
+      if (this.citySelection) return;
+
+      if (!this.locationInputStarted) {
+        // If input hasn't started yet
+        this.locationInputStarted = true;
+
+        // Save the value
+        this.locationInputValue = this.$locationInput.val();
+
+        // Schedule next check
+        this.locationTimer = setTimeout(this.throttleInput, 1500);
+      }
+    });
+
+    // Handle city selection from dropdown
+    this.$locationDropdownMenu.click((event) => {
+      let target = event.target;
+
+      if (target.tagName !== "LI") return;
+
+      let dataset = target.dataset;
+
+      this.citySelection = true;
+      clearTimeout(this.locationTimer);
+
+      // Save attributes from selected city
+      this.$locationInput
+        .attr("data-lat", dataset.lat)
+        .attr("data-lon", dataset.lon)
+        .attr("data-name", dataset.name)
+        .val(dataset.name);
+
+      this.citySelection = false;
+      this.locationInputStarted = false;
+      this.newValue = null;
+
+      this.$locationDropdownMenu.empty();
     });
   },
 
   async getCities({ headers, endpoint, method }) {
-    let result = await this.makeRequest({
+    return await this.makeRequest({
       headers,
       endpoint,
       method,
     });
-    console.log(result);
+  },
+
+  async throttleInput() {
+    // Cache request information
+    let requestInfo = this.requests.location;
+
+    let newValue = this.$locationInput.val();
+
+    if (!(newValue === this.locationInputValue)) {
+      // If the inputed value has been changed during the last 2000ms
+      // Save new value
+      this.locationInputValue = newValue;
+
+      // Adjust searchParams
+      requestInfo.endpoint.searchParams.set("city", this.locationInputValue);
+
+      // Make request
+      let cities = await this.getCities({
+        headers: requestInfo.headers,
+        endpoint: requestInfo.endpoint,
+        method: requestInfo.method,
+      });
+
+      // Schedule next check
+      this.locationTimer = setTimeout(this.throttleInput, 2500);
+
+      this.displayCities(cities);
+    } else {
+      this.locationInputStarted = false;
+    }
+  },
+
+  displayCities(cities) {
+    cities.forEach((city) => {
+      this.$locationDropdownMenu
+        .append(
+          $("<li></li>")
+            .addClass("dropdown-item")
+            .attr("data-lat", city.lat)
+            .attr("data-lon", city.lon)
+            .attr("data-name", city["display_name"])
+            .text(city["display_name"])
+        )
+        .append($("<li></li>").addClass("dropdown-divider"));
+    });
+
+    this.$locationDropdownToggle.dropdown("toggle");
   },
 };
