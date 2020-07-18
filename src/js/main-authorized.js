b18d65d65c2c943ca58d35b29e84048b768a1f75
Irina <irensaywhen@gmail.com>
2020-07-18 08:18:24 +0300

    Making search form

--- src/js/main-authorized.js
+++ src/js/main-authorized.js
@@ -1,9 +1,9 @@
 import PhotoUploader from "./modules/uploader.js";
 import PhotoEditor from "./modules/editor.js";
-//import Form from "./modules/form.js";
 import Boost from "./modules/boost.js";
+import SearchProfilesForm from "./modules/search-profiles-form.js";
 
 window["PhotoUploader"] = PhotoUploader;
 window["PhotoEditor"] = PhotoEditor;
-//window["Form"] = Form;
 window["Boost"] = Boost;
+window["SearchProfilesForm"] = SearchProfilesForm;
---
+++ src/js/modules/search-profiles-form.js
@@ -0,0 +1,10 @@
+import Form from "./form.js";
+
+export default class SearchProfilesForm extends Form {
+  constructor(options) {
+    super(options);
+    alert("Making searching form!");
+  }
+  cacheElements() {}
+  setUpEventListeners() {}
+}
