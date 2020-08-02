d1b2c379b8b722b61e011977abb8a9d4bf607c66
Irina <irensaywhen@gmail.com>
2020-08-02 18:48:52 +0300

    Chat initial commit

--- src/js/main-authorized.js
+++ src/js/main-authorized.js
@@ -4,6 +4,7 @@ import Boost from "./modules/boost.js";
 import SearchProfilesForm from "./modules/search-profiles-form.js";
 import BuyPremiumForm from "./modules/buyPremiumForm.js";
 import ChatList from "./modules/chatList.js";
+import Gallery from "./modules/gallery.js";
 
 window["PhotoUploader"] = PhotoUploader;
 window["PhotoEditor"] = PhotoEditor;
@@ -11,3 +12,4 @@ window["Boost"] = Boost;
 window["SearchProfilesForm"] = SearchProfilesForm;
 window["BuyPremiumForm"] = BuyPremiumForm;
 window["ChatList"] = ChatList;
+window["Gallery"] = Gallery;
