b24aae4b179c7825901ae6955d8cbeebcbb92d79
Irina <irensaywhen@gmail.com>
2020-09-05 15:07:52 +0300

    Progress indicator for file read

--- src/js/modules/avatar.js
+++ src/js/modules/avatar.js
@@ -15,66 +15,39 @@ export default class Avatar extends EditorModal {
     // Set configuration for upload type
     this.configuration.avatar = true;
 
-    // Create FileReader instance to handle reading image data
-    this.reader = new FileReader();
-
     // Binding context
     this._cacheElements = this._cacheElements.bind(this);
     this._setUpEventListeners = this._setUpEventListeners.bind(this);
-    this.previewAvatar = this.previewAvatar.bind(this);
     this.submitAvatar = this.submitAvatar.bind(this);
     this.updateMarkup = this.updateMarkup.bind(this);
-    this.generateFormData = this.generateFormData.bind(this);
 
-    // Cache elements according to passed selectors
+    // Class usage preparation
     this._cacheElements();
-
-    // Setup event listeners
     this._setUpEventListeners();
 
     // Prepare avatar for using FileReader to load and preview photos
     Object.assign(Avatar.prototype, fileReaderMixin);
-    this.initializeFileReader(this.$form);
+
+    this.initializeFileReader({ form: this.$form, modal: this.$modal });
   }
 
-  /**
-   * Function caches elements according to passed options.
-   */
   _cacheElements() {
     super._cacheElements();
     let selectors = this.selectors;
 
     // Avatar elements in the markup
     this.$avatar = $(selectors.elementSelector);
-
     // Avatar preview
     this.$avatarPreview = this.$modal.find(selectors.preview);
-
     // Save previous avatar to discard changes if user doesn't submit the form
     this.prevAvatarLink = this.$avatarPreview.attr('src');
-
-    // Inputs
-    // There will be not need in this line after integrating FileReader
-    this.$avatarInputs = this.$modal.find(selectors.input);
   }
 
-  /**
-   * Function setup event listeners on the initialization stage of the object creation
-   */
   _setUpEventListeners() {
     super._setUpEventListeners();
-    // Setup event handler for loading of the image data event
-    this.reader.onload = e => {
-      // For testing purposes, set it here
-      this._preview(event.target);
-    };
 
-    // Listen to changes on the input elements
-    this.$avatarInputs.change(event => {
-      this.previewAvatar(event.target);
-    });
-
-    // Submit avatar
+    // Here we need to listen to submit event also
+    // Add it here
     this.$form.submit(event => {
       event.preventDefault();
 
@@ -82,6 +55,13 @@ export default class Avatar extends EditorModal {
     });
   }
 
+  /**
+   * Function specific to classes using FileReader Mixin.
+   * It handles class-specific functionality required for preview
+   * Here, it saves src and sets the loaded photo in preview container
+   * @param {FileReader Object} fileReader - the resulting fileReader object
+   * to preview loaded photo
+   */
   _preview(fileReader) {
     // cache
     let src = fileReader.result;
@@ -89,22 +69,6 @@ export default class Avatar extends EditorModal {
     this.$avatarPreview.attr('src', src);
     // save src to update markup
     this.newAvatarLink = src;
-  }
-
-  /**
-   * This function is called when the process of avatar preview is occuring.
-   * It accepts the input field from which the avatar is being upload
-   * The function starts loading the image
-   * @param {DOMElement} input
-   */
-  previewAvatar(input) {
-    if (input.files && input.files[0]) {
-      //Save the currently selected avatar
-      this.avatar = input.files[0];
-
-      //Start reading the image from the input
-      this.reader.readAsDataURL(input.files[0]);
-    }
   }
 
   /**
@@ -125,17 +89,10 @@ export default class Avatar extends EditorModal {
     this.uploaded = false;
   }
 
-  /**
-   * Function updates the avatar in the editing area and in the menu
-   */
   updateMarkup() {
     this.$avatar.attr('src', this.newAvatarLink);
   }
 
-  /**
-   * Function delete the newly uploaded avatar
-   * If user didn't submit the form
-   */
   discardChanges() {
     this.$avatarPreview.attr('src', this.prevAvatarLink);
   }
--- src/js/modules/fileReaderMixin.js
+++ src/js/modules/fileReaderMixin.js
@@ -1,13 +1,16 @@
 // Here we'll make mixin and put functions on the global level
 // but export only one of them
+import Handlebars from 'handlebars';
 
 export default {
-  initializeFileReader($photosForm) {
-    console.log('Initializing filereader!');
+  initializeFileReader({ form, modal }) {
     // Bind context
     _setReaderEventListeners = _setReaderEventListeners.bind(this);
     _readFile = _readFile.bind(this);
     _loadfromInput = _loadfromInput.bind(this);
+    _showProgress = _showProgress.bind(this);
+    _prepareTemplate = _prepareTemplate.bind(this);
+    _insertProgressBar = _insertProgressBar.bind(this);
     _cacheElements = _cacheElements.bind(this);
     _setUpEventLiseners = _setUpEventLiseners.bind(this);
 
@@ -15,59 +18,38 @@ export default {
     this._preview = this._preview.bind(this);
 
     // Save passed arguments
-    $form = $photosForm;
+    [$form, $modal] = [form, modal];
 
-    // Perform caching and setting event listeners
-    // Maybe there will be no need in caching function
+    // Preparations to read file
     _cacheElements();
     _setUpEventLiseners();
   },
 };
 
 // Private varialbes
-let selectors, $form, classes;
+let selectors,
+  fileRead,
+  classes,
+  $modal,
+  $form,
+  progressTemplate,
+  $progressContainer,
+  $progressBar;
 
 // Private functions
-/**
- * Function setting event listeners to all of the
- * @param {FileReader Object} reader - reader to set event listeners to
- */
-function _setReaderEventListeners(reader) {
-  reader.addEventListener('loadstart', event => {
-    // Show loading indicator here
-    console.log('Loading start');
-    console.log(event);
-  });
-
-  reader.addEventListener('loadend', event => {
-    //Delete progress indicator here
-    console.log('Loading end');
-    console.log(event);
-  });
-
-  reader.addEventListener('load', event => {
-    console.log('load event');
-    // While previewing avatar, we can save the URL
-    // as _preview function will be implemented in each class
-    //_preview(event.target);
-    console.log(event.target);
-  });
-
-  reader.addEventListener('error', event => {
-    console.log('Error');
-  });
-
-  reader.addEventListener('abort', event => {
-    console.log('Aborted');
-  });
-}
 
 /**
  * Function to cache elements required for FileReader to work
  */
 function _cacheElements() {
   selectors = this.selectors;
+  fileRead = selectors.fileRead;
   classes = this.classes;
+
+  // Progress indicator
+  $progressContainer = $modal.find(fileRead.progress);
+  // Template
+  progressTemplate = document.getElementById(fileRead.templateId);
 }
 
 /**
@@ -77,28 +59,21 @@ function _cacheElements() {
 function _setUpEventLiseners() {
   $form.on('change', event => {
     let target = event.target;
-    console.log(target.classList);
 
     // Stop execution if the target is not for photo upload
     if (!target.classList.contains(classes.input)) return;
 
     _loadfromInput(target);
   });
-}
 
-/**
- * Function to read file and start loading it
- * It instantiates a FileReader Object for the current file
- * Sets event listeners to listen to events on reader
- * And then, starts reading it to generate URL
- * @param {File Object} file
- */
-function _readFile(file) {
-  console.log('Reading file!');
-  let reader = new FileReader();
+  // Hide loading indicator after transition
+  $modal.on('transitionend', event => {
+    let $target = $(event.target);
 
-  _setReaderEventListeners(reader);
-  reader.readAsDataURL(file);
+    if (!$target.hasClass('loadend')) return;
+    // Remove progress indicator
+    $target.closest(fileRead.fileProgressWrapper).remove();
+  });
 }
 
 /**
@@ -108,12 +83,109 @@ function _readFile(file) {
  * @param {DOMElement} input - input element from which all the files are loaded
  */
 function _loadfromInput(input) {
-  console.log('Loading from input!');
   let files = input.files;
 
   if (!files[0]) return;
 
   for (let file of files) {
-    _readFile(file);
+    // For now leave it without filename
+    let $progressBar = _insertProgressBar({ fileName: file.name });
+    // Read file and connect it with the current
+    _readFile({ file, $progressBar });
   }
+}
+
+/**
+ * Function to read file and start loading it
+ * It instantiates a FileReader Object for the current file
+ * Sets event listeners to listen to events on reader
+ * And then, starts reading it to generate URL
+ * @param {File Object} file
+ */
+function _readFile({ file, $progressBar }) {
+  let reader = new FileReader();
+  // Save progress bar for the current reader
+  reader.$progressBar = $progressBar;
+  // Prepare reader fo reading file
+  _setReaderEventListeners(reader);
+  // Read file
+  reader.readAsDataURL(file);
+}
+
+/**
+ * Function copying template
+ * and compiling it with provided filename
+ * @param {String} fileName - name of the file being loaded
+ */
+function _prepareTemplate(fileName) {
+  // Get template content
+  let progress = progressTemplate.innerHTML;
+
+  // Compile template with provided filename
+  progress = Handlebars.compile(progress);
+  progress = progress({ name: fileName });
+
+  return progress;
+}
+
+/**
+ * Function inserting progress bar
+ * @param {String} fileName - name of the file being loaded
+ */
+function _insertProgressBar({ fileName }) {
+  // Prepare template for insertion
+  let template = _prepareTemplate(fileName);
+  // Insert the template into the progress container
+  $progressContainer.append(template);
+  // Save progress bar
+  $progressBar = $progressContainer.find(fileRead.progressBar);
+  return $progressBar;
+}
+
+/**
+ * Function showing progress of photo read
+ * in the progress bar
+ * @param {Number} loaded - amount of loaded bytes
+ * @param {Number} total - amount of total bytes to load
+ */
+function _showProgress({ loaded, total }) {
+  // Calculate progress
+  let progress = Math.round((loaded / total) * 100);
+  // Update progress
+  $progressBar.css('width', `${progress}%`);
+}
+
+/**
+ * Function setting event listeners to the current reader
+ * @param {FileReader Object} reader - reader to set event listeners to
+ */
+function _setReaderEventListeners(reader) {
+  reader.addEventListener('loadstart', event => {
+    // Show progress indicator
+    //$progress.show();
+  });
+
+  reader.addEventListener('progress', event => {
+    _showProgress({ loaded: event.loaded, total: event.total });
+  });
+
+  reader.addEventListener('loadend', event => {
+    let target = event.target;
+
+    // Indicate that the file related to the current progress bar was loaded
+    target.$progressBar.addClass('loadend');
+  });
+
+  reader.addEventListener('load', event => {
+    let target = event.target;
+    setTimeout(this._preview, 500, target);
+    // Show submit button
+    this.$modalFooter.show();
+  });
+
+  reader.addEventListener('preview', event => {});
+
+  reader.addEventListener('error', event => {
+    console.log('Error');
+  });
 }
