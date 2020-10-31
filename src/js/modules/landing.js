79d06eadfb1552ec18d538866e0e4893d800c467
Irina <irensaywhen@gmail.com>
2020-10-31 10:11:50 +0300

    Showing signup modal when profile is clicked set

--- src/js/modules/landing.js
+++ src/js/modules/landing.js
@@ -6,21 +6,46 @@ export default class Landing extends ServerRequest {
     // Mimick private variables here. Apply all the knowledge you gained
     let { config } = options;
 
-    console.log(config);
+    const $document = $(document);
 
-    let {
+    /**
+     * Password recovery
+     * 1. Cache elements
+     * 2. Set event listener to show password recovery modal
+     * when "Forgot password" is clicked
+     */
+    const {
       forgotPasswordBtn,
       recoveryPasswordModal,
     } = this.selectors.passwordRecovery;
 
-    let $recoveryPasswordModal = $(recoveryPasswordModal),
+    const $recoveryPasswordModal = $(recoveryPasswordModal),
       $forgotPasswordBtn = $(forgotPasswordBtn);
 
+    // Show password recovery modal
     $forgotPasswordBtn.click(event => {
       event.preventDefault();
 
       $forgotPasswordBtn.closest('.modal').modal('hide');
       $recoveryPasswordModal.modal('show');
     });
+
+    /**
+     * Show signup modal when profiles are clicked
+     * 1. Cache signup modal and save selector for profile
+     * 2. If profile is clicked, show signup modal
+     */
+    if (config.profiles) {
+      const profile = this.selectors.profile || '.show-signup',
+        $signUpModal = $(this.selectors.signUpModal);
+
+      $document.click(event => {
+        let $target = $(event.target);
+
+        if ($target.closest(profile).length === 0) return;
+
+        $signUpModal.modal('show');
+      });
+    }
   }
 }
